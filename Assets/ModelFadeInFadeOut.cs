9b1894b336807592665a11ee8ba1b64f6d1248b8
RaoulBickmann <raoul.bickmann@googlemail.com>
2017-03-09 23:18:02 +0100

    Started on leveldesign

---
+++ Assets/ModelFadeInFadeOut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelFadeInFadeOut : MonoBehaviour {
+
+    // Lets an object fade in out
+
+    public float fadeSpeed = 1.0f;
+    public float maxAlpha = 0.2f;
+
+    private MeshRenderer meshRenderer;
+    private Color maxAlphaColor;
+    private Color minAlphaColor;
+
+	void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+        maxAlphaColor = meshRenderer.material.color;
+        minAlphaColor = maxAlphaColor;
+        minAlphaColor.a = maxAlpha;
+	}
+	
+	void Update () {
+        meshRenderer.material.color = Color.Lerp(minAlphaColor, maxAlphaColor, Mathf.PingPong(Time.time * fadeSpeed, 1));
+    }
+}
