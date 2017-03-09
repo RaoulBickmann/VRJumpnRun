18ad693ec1510e8bd7dedfcc0d010f4bcb83b3e2
RaoulBickmann <raoul.bickmann@googlemail.com>
2017-03-10 00:24:32 +0100

    Removed Bullet Collision, Built turretwheel

---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    private Transform transform;
+    public Vector3 rotation;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    transform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotation);
+	}
+}
